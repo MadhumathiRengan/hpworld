caf6cab211985b12164af9fc9ceba65d0e347b8c
MadhumathiRengan <madhumathi5493@gmail.com>
2021-11-09 13:01:09 +0530

    Password Functionality

--- src/org/test/FacebookLogin.java
+++ src/org/test/FacebookLogin.java
@@ -1,5 +1,13 @@
 package org.test;
 
 public class FacebookLogin {
+	
+	public static void password() {
+		
+		System.out.println("User Name");
+	}
 
+	public static void main(String[] args) {
+		password();
+	}
 }
